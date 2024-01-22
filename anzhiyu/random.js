538875d49f37c45e4dd536a2192998ac33a1a525
huozhebuhao <wx2273254832@163.com>
2024-01-22 01:47:54 -0600

    Site updated: 2024-01-22 01:47:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/20/如何搭建个人博客/","2024/01/18/hello-world/"];function toRandomPost(){
+var posts=["2024/01/18/hello-world/","2024/01/20/如何搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
